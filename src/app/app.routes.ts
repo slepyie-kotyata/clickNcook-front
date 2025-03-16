46c8f02b6d238449e87cda0ee4603b189f5a7ea6
Son1kXDev <son1kx@vk.com>
2025-03-16 18:03:07 +0300

    feat: страница авторизации

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,16 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
+import { LoginLayoutComponent } from './shared/ui/login-layout/login-layout.component';
 
-export const routes: Routes = [{ path: 'auth', component: AuthComponent }];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginLayoutComponent,
+    children: [
+      {
+        path: 'auth',
+        component: AuthComponent,
+      },
+    ],
+  },
+];
