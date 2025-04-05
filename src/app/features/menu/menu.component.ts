41c41cf5d79dbba9d2a402d8c88a2ba5b9be60b1
Son1kXDev <son1kx@vk.com>
2025-04-05 19:38:40 +0300

    feat: меню под мобильные устройства

--- src/app/features/menu/menu.component.ts
+++ src/app/features/menu/menu.component.ts
@@ -1,17 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import formatNumber from '../../shared/lib/formatNumber';
 import { MenuButtonComponent } from '../../shared/ui/menu-button/menu-button.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [MenuButtonComponent],
+  imports: [MenuButtonComponent, NgIf],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css',
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
   @Input() dishesCount: number = 0; //TODO: get from api
   @Input() cashCount: number = 0; //TODO: get from api
+
+  isOpen: boolean = false;
+  protected readonly window = window;
 
   getDishCount(): string {
     return formatNumber(this.dishesCount);
@@ -19,5 +23,14 @@ export class MenuComponent {
 
   getCashCount(): string {
     return formatNumber(this.cashCount);
+  }
+
+  toggleMenu(): void {
+    this.isOpen = !this.isOpen;
+    console.log('toggle');
+  }
+
+  ngOnInit(): void {
+    console.log(window.innerWidth);
   }
 }
