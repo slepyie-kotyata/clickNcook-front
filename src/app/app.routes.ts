e502231b5bf2aa19c8fbfffbd44d86e64da06fea
Son1kXDev <son1kx@vk.com>
2025-04-05 19:26:05 +0300

    refactor: перенос логики игрового интерфейса

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,12 @@ import { AuthComponent } from './pages/auth/auth.component';
 import { LoginLayoutComponent } from './shared/ui/login-layout/login-layout.component';
 import { RegComponent } from './pages/reg/reg.component';
 import { authGuard } from './shared/lib/guards/auth.guard';
-import { GameplayLayoutComponent } from './shared/ui/gameplay-layout/gameplay-layout.component';
+import { GameComponent } from './pages/game/game.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: GameplayLayoutComponent,
+    component: GameComponent,
     canActivate: [authGuard],
   },
   {
---
+++ src/app/pages/game/game.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { MenuComponent } from '../../features/menu/menu.component';
+import formatNumber from '../../shared/lib/formatNumber';
+
+@Component({
+  selector: 'app-game',
+  standalone: true,
+  imports: [MenuComponent],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.css',
+})
+export class GameComponent {
+  prestigeLvl: number = 0; //TODO: get from api
+  playerLvl: number = 15; //TODO: get from api
+
+  dishesCount: number = 0; //TODO: get from api
+  moneyCount: number = 0; //TODO: get from api
+
+  private cookClickCount = 0;
+  private sellClickCount = 0;
+
+  handleCook(): void {
+    this.cookClickCount++;
+    this.dishesCount++;
+
+    if (this.cookClickCount > 5) {
+      //TODO: api post/cook
+      this.cookClickCount = 0;
+      return;
+    }
+  }
+
+  handleSell(): void {
+    if (this.dishesCount <= 0) return;
+
+    this.sellClickCount++;
+    this.dishesCount--;
+    this.moneyCount++; //заглушка
+    if (this.sellClickCount > 5) {
+      //TODO: api post/cook
+      this.sellClickCount = 0;
+      return;
+    }
+  }
+
+  protected getPrestigeLvl(): string {
+    return formatNumber(this.prestigeLvl);
+  }
+
+  protected getPlayerLvlPercentage(): number {
+    let nextLvl = 100; //TODO: get from api
+    return (this.playerLvl / nextLvl) * 100;
+  }
+}
