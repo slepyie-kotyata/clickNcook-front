21be6dacbb2d21ab0e83cc4fe79fdcf919c1d991
Son1kXDev <son1kx@vk.com>
2025-05-21 16:56:49 +0300

    chore: отключено контекстное меню ПКМ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Click & Cook';
+
+  @HostListener('document:contextmenu', ['$event'])
+  onRightClick(event: MouseEvent) {
+    event.preventDefault();
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeydown(event: KeyboardEvent) {
+    if (
+      (event.ctrlKey || event.metaKey) &&
+      ['c', 'u', 's', 'a'].includes(event.key.toLowerCase())
+    ) {
+      event.preventDefault();
+    }
+  }
 }
