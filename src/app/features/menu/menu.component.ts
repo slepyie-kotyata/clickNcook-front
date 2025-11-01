2426e335632fe7e3ceed5c29a7c7690c976f01ba
Igor <son1kx@vk.com>
2025-11-01 11:10:32 +0300

    fix: improve mobile upgrade access

--- src/app/features/menu/menu.component.ts
+++ src/app/features/menu/menu.component.ts
@@ -1,20 +1,23 @@
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import formatNumber from '../../shared/lib/formatNumber';
 import { MenuButtonComponent } from '../../shared/ui/menu-button/menu-button.component';
 import { UpgradeWindowComponent } from '../../widgets/upgrade-window/upgrade-window.component';
 import { GameService } from '../../shared/lib/services/game/game.service';
 import { Upgrade } from '../../entities/types';
-import { NgForOf } from '@angular/common';
+import { NgForOf, NgIf } from '@angular/common';
 import { SessionService } from '../../shared/lib/services/game/session.service';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [MenuButtonComponent, UpgradeWindowComponent, NgForOf],
+  imports: [MenuButtonComponent, UpgradeWindowComponent, NgForOf, NgIf],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css',
 })
 export class MenuComponent {
+  @Input() showCloseButton = false;
+  @Output() close = new EventEmitter<void>();
+
   protected menuButtons: {
     type: Upgrade;
     icon: string;
@@ -46,5 +49,9 @@ export class MenuComponent {
 
   selectMenu(value: Upgrade) {
     this.gameService.selectMenuType(value);
+  }
+
+  onClose() {
+    this.close.emit();
   }
 }
