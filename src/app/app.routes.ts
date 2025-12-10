79f37a82cafbb09ceb151d1583b7a365fe4a012a
Son1kXDev <son1kx@vk.com>
2025-12-10 17:05:03 +0300

    chore: удален неиспользуемый mobileGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,23 @@
-import { Routes } from '@angular/router';
-import { AuthComponent } from './pages/auth/auth.component';
-import { LoginLayoutComponent } from './shared/ui/login-layout/login-layout.component';
-import { RegComponent } from './pages/reg/reg.component';
-import { authGuard } from './shared/lib/guards/auth.guard';
-import { GameComponent } from './pages/game/game.component';
-import { mobileGuard } from './shared/lib/guards/mobile.guard';
+import {Routes} from '@angular/router';
+import {AuthComponent} from './pages/auth/auth.component';
+import {LoginLayoutComponent} from './shared/ui/login-layout/login-layout.component';
+import {RegComponent} from './pages/reg/reg.component';
+import {authGuard} from './shared/lib/guards/auth.guard';
+import {GameComponent} from './pages/game/game.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: GameComponent,
-    canActivate: [authGuard, mobileGuard],
+    canActivate: [authGuard],
   },
   {
     path: '',
     component: LoginLayoutComponent,
     canDeactivate: [authGuard],
     children: [
-      { path: 'auth', component: AuthComponent },
-      { path: 'reg', component: RegComponent },
+      {path: 'auth', component: AuthComponent},
+      {path: 'reg', component: RegComponent},
     ],
   },
 ];
