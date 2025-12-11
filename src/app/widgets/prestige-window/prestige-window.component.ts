e4fe548d0aa3da6794f3539f1a14cd798252bcfd
Son1kXDev <son1kx@vk.com>
2025-12-12 00:15:21 +0300

    fix: подсчет накопленного множителя престижа

--- src/app/widgets/prestige-window/prestige-window.component.ts
+++ src/app/widgets/prestige-window/prestige-window.component.ts
@@ -22,13 +22,12 @@ export class PrestigeWindowComponent {
 
   protected get accumulatedPrestigeMultiplier(): number {
     return parseFloat(
-      ((this.store.session()?.prestige.current_boost_value ?? 1) + 1 +
+      ((this.store.session()?.prestige.current_boost_value ?? 1) +
         (this.store.session()?.prestige.accumulated_value ?? 0) * 0.5).toFixed(2),
     );
   }
 
   protected close() {
-    console.log(this.store.session()?.prestige.accumulated_value);
     this.closeEvent.emit(false);
   }
 
